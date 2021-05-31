5c65611a13fbe89bdf7e91b52f7686c7ae498726
Yukaru <57414313+Yukaru-san@users.noreply.github.com>
2021-05-31 20:56:24 +0200

    Update Utils.java

--- Java/Utils.java
+++ Java/Utils.java
@@ -2,8 +2,7 @@ package utils;
 import java.awt.Point;
 
 public class Utils {
-	
-	public static boolean pointsAreEqual(Point p1, Point p2) {	
+	public static boolean arePointsEqual(Point p1, Point p2) {	
 		if (p1.x != p2.x || p1.y != p2.y)
 			return false;
 		
